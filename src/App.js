5a09125ee3a570a19d76cdc1671de17d3b1462a2
Marius Tulbure <marius@figshare.com>
2017-11-19 03:43:57 +0200

    :wrench: Setup the main pages
    Signed-off-by: Marius Tulbure <marius@figshare.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Homepage from "./homepage";
+import Login from "./login";
+
 import "./App.css";
 
-class App extends Component {
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">PITCH you</h1>
-        </header>
-        <p className="App-intro">
-          To be announced soon
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">PITCH you</h1>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+
+            <hr />
+          </header>
+
+          <p className="App-intro">
+            <div>
+              <Route exact={true} path="/" component={Homepage} />
+              <Route path="/login" component={Login} />
+            </div>
+          </p>
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
